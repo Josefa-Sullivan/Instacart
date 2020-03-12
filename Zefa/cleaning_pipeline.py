d11ba352a9d89764408f3515726a53d214b6fd87
Josefa-Sullivan <josefa.sullivan@gmail.com>
2020-03-12 19:04:01 -0400

    ZS: test K-means and PCA

---
+++ Zefa/cleaning_pipeline.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+products = pd.read_csv('../data/products.csv')
+orders = pd.read_csv('../data/orders.csv')
+departments = pd.read_csv('../data/departments.csv')
+aisles = pd.read_csv('../data/aisles.csv')
+train = pd.read_csv('../data/order_products__train.csv')
+prior = pd.read_csv('../data/order_products__prior.csv')
+
+
+# Combine Orders from train and prior set because we are not concerned with the Kaggle Objective
+# Shape = (33819106, 4)
+priorTrain = pd.concat([prior, train])
+
+# Merge Orders with priorTrain to include product information, Shape = (33819106, 9)
+# Contains order_id, product_id, add_to_cart_order, reordered, user_id, order_number, order_dow, order_hour_of_day, days_since_prior_order
+all_orders = pd.merge(priorTrain, orders, how="left", left_on="order_id", right_on="order_id").drop(['eval_set'], axis=1)
+
+
+## Merge aisle, department and products into one "goods" dataframe, Shape = (49688, 6)
+goods = pd.merge(left=pd.merge(left=products, right=departments, how='left'), right=aisles, how='left')
+
+# Replace spaces in product name and aisle name with '_' but retain '-' 
+goods.product_name = goods.product_name.str.replace(' ', '_').str.lower() 
+goods.aisle = goods.aisle.str.replace(' ', '_').str.lower() 
+goods.department = goods.department.str.replace(' ', '_').str.lower() 
+
+# Create a dataframe with all information on orders, users, and  products
+full_prod_orders = pd.merge(all_orders,goods, on = ['product_id','product_id'])
